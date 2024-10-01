49c77faa55385b1c62a7e80781682b75f4915197
jedi1118 <pobox.uno@gmail.com>
2024-10-01 14:37:06 -0700

    add notes in page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>Image gallery with thumnail carousel in React.</div>
+        <div>images loaded from pexels.com</div>
         <div>Due to large image size, click on a thumbnail will take a sec or so to load the image in main panel</div>
         <Gallery images={IMAGES}/>
       </header>
